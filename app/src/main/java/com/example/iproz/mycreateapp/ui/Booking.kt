bf0b41f3d86d79311e5b1546880b2ec3097557db
iproz <nanthakarn18@gmail.com>
2018-11-27 21:01:05 +0700

    Hello World

--- app/src/main/java/com/example/iproz/mycreateapp/ui/Booking.kt
+++ app/src/main/java/com/example/iproz/mycreateapp/ui/Booking.kt
@@ -2,6 +2,7 @@ package com.example.iproz.mycreateapp.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.iproz.mycreateapp.R
 
 class Booking : AppCompatActivity() {
@@ -9,5 +10,7 @@ class Booking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_LONG).show()
     }
 }
